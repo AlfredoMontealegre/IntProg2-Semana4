71f56de1a0ed4f6a5c15e8bdf9cd8a9886cec96a
AlfredoMontealegre <montealfredo24@gmail.com>
2025-04-08 08:29:24 -0600

    Ejercicios de Python

---
+++ ejercicios_py/ejercicio1.py
@@ -0,0 +1,12 @@
+# Declarar las variables
+nombre_completo = "Alfredo de Jesus Montealegre Ocampo"
+edad = 17
+altura = 1.75
+soltero = True
+
+print(f"Nombre completo: {nombre_completo}")
+print(f"Tipo de dato: {type(nombre_completo)}")
+
+print(f"{edad} {type(edad)}")
+print(f"{altura:.2f} {type(altura)}")
+print(f"{soltero} {type(soltero)}")
